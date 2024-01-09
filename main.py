29e604b11cf4e512a04857682d5edd6c4880bed9
allan <sequeira_36@hotmail.com>
2024-01-09 20:20:26 +0100

    tento je projekt na class z OOP

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #constructor
 class Person:
-    def __init__(self, name, age, gender, occupation, addresa, vysku):
-        self.name = name
-        self.age = age
+    def __init__(self, name, age, gender, occupation, addresa, vysku, farba_oci):
+        self.__nombre = name #el nombre senala a el atributo que definimos arriva
+        self.edad = age
         self.gender = gender
         self.occupation = occupation
         self.addresa = addresa
         self.vysku = vysku
-
+        self.farba_oci = farba_oci
     def pozdrav(self):
-        print(f"ahoj volam sa {self.name} a man {self.age} rokov ")
+        print(f"ahoj volam sa {self.__nombre} a man {self.edad} rokov , a mam {self.farba_oci} oci")
     def rod(self):
         print(f"som {self.gender}")
     def vydlisko(self):
@@ -17,15 +17,21 @@ class Person:
     def profesia(self):
         print(f" a som {self.occupation}")
     def zostarnem(self, cislo):
-        self.age = self.age + cislo
+        self.edad = self.edad + cislo
+allan = Person("Allan", 37, "muz", "Ekonom", "Bratislava", "1.67", "hnede")
 
-allan = Person("Allan", 37, "muz", "Ekonom", "Bratislava", "1.67")
+allan.pozdrav()
+
+
+''''
 allan.pozdrav()
 allan.vydlisko()
 allan.profesia()
 allan.rod()
-print(allan.age)
+print(allan.farba_oci)
+print(allan.vysku)
+print(allan.edad)
 allan.zostarnem(10)
-print(f"so {allan.age} stary")
+print(f"so {allan.edad} starsi")'''
 
 
