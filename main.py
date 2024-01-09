733fbf69fe2bd6da09f52d30006c48ae5f6b7a88
allan <sequeira_36@hotmail.com>
2024-01-09 20:20:26 +0100

    tento je projekt na class z OOP

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 class Person:
     def __init__(self, name, age, gender, occupation, addresa, vysku, farba_oci):
         self.__nombre = name #el nombre senala a el atributo que definimos arriva
-        self.edad = age
+        self.__edad = age
         self.gender = gender
         self.occupation = occupation
         self.addresa = addresa
         self.vysku = vysku
         self.farba_oci = farba_oci
     def pozdrav(self):
-        print(f"ahoj volam sa {self.__nombre} a man {self.edad} rokov , a mam {self.farba_oci} oci")
+        print(f"ahoj volam sa {self.__nombre} a man {self.__edad} rokov , a mam {self.farba_oci} oci")
     def rod(self):
         print(f"som {self.gender}")
     def vydlisko(self):
@@ -17,7 +17,7 @@ class Person:
     def profesia(self):
         print(f" a som {self.occupation}")
     def zostarnem(self, cislo):
-        self.edad = self.edad + cislo
+        self.__edad = self.__edad + cislo
 allan = Person("Allan", 37, "muz", "Ekonom", "Bratislava", "1.67", "hnede")
 
 allan.pozdrav()
